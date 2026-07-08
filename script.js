fb6a0a55e2f70841d692531993db627a3fd40347
Digital Hub <akangbekamal06@gmail.com>
2026-07-08 23:38:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -839,31 +839,7 @@ counter.innerText=target;
 update();
 
 });
-/* ==========================
-   LOADER
-========================== */
 
-window.addEventListener("load",()=>{
-
-const loader=document.getElementById("loader");
-
-if(loader){
-
-setTimeout(()=>{
-
-loader.style.opacity="0";
-
-setTimeout(()=>{
-
-loader.remove();
-
-},500);
-
-},700);
-
-}
-
-});
 /* ==========================
    SCROLL ANIMATION
 ========================== */
