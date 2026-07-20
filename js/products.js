7b000fefcfeaf4f0a890f83a9f267f114b425246
Digital Hub <akangbekamal06@gmail.com>
2026-07-20 22:31:25 +0000

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,35 @@
+const products = [
 
+{
+id:1,
+name:"Netflix Premium",
+category:"Subscription",
+price:35,
+image:"images/netflix.png"
+},
+
+{
+id:2,
+name:"Spotify Premium",
+category:"Subscription",
+price:40,
+image:"images/spotify.png"
+},
+
+{
+id:3,
+name:"ExpressVPN",
+category:"VPN",
+price:45,
+image:"images/expressvpn.png"
+},
+
+{
+id:4,
+name:"NordVPN",
+category:"VPN",
+price:45,
+image:"images/nordvpn.png"
+}
+
+];
