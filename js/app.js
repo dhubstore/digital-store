29cc1e0aa2717e6b2fcb73681d70b94d14d9b1b2
Digital Hub <akangbekamal06@gmail.com>
2026-07-21 17:45:07 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -515,3 +515,24 @@ behavior:"smooth"
 });
 
 };
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+document.querySelectorAll("section").forEach(section=>{
+
+section.classList.add("fade-up");
+
+observer.observe(section);
+
+});
