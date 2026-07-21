d49fde68fc4f56ec170ba04b8ff378c79eeddd41
Digital Hub <akangbekamal06@gmail.com>
2026-07-21 09:58:58 +0000

    Update products.js

--- js/products.js
+++ js/products.js
@@ -33,3 +33,50 @@ image:"images/nordvpn.png"
 }
 
 ];
+{
+id:1,
+name:"Netflix Premium",
+category:"subscriptions",
+price:35,
+image:"images/netflix.png"
+},
+
+{
+id:2,
+name:"Spotify Premium",
+category:"subscriptions",
+price:40,
+image:"images/spotify.png"
+},
+
+{
+id:3,
+name:"ExpressVPN",
+category:"vpn",
+price:45,
+image:"images/expressvpn.png"
+},
+
+{
+id:4,
+name:"NordVPN",
+category:"vpn",
+price:45,
+image:"images/nordvpn.png"
+},
+
+{
+id:5,
+name:"$10 iTunes Gift Card",
+category:"giftcards",
+price:120,
+image:"images/itunes.png"
+},
+
+{
+id:6,
+name:"10GB MTN Data",
+category:"data",
+price:50,
+image:"images/data.png"
+}
