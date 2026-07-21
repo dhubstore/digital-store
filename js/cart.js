b6ca60e1d658fea62381b8e13e6eecce911d5b45
Digital Hub <akangbekamal06@gmail.com>
2026-07-21 15:11:43 +0000

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,13 @@
+let cart=[];
 
+function addToCart(id){
+
+const product=products.find(p=>p.id===id);
+
+cart.push(product);
+
+document.querySelector(".cart-count").textContent=cart.length;
+
+alert(product.name+" added to cart!");
+
+}
