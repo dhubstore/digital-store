17e70ff5e09daf21cdbeb3473ad73ad48c0d75f8
Digital Hub <akangbekamal06@gmail.com>
2026-07-23 17:10:52 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+// ============================
+// CATEGORY CLICK
+// ============================
 
+document.querySelectorAll(".category-card").forEach(card=>{
+
+card.addEventListener("click",()=>{
+
+const category=card.dataset.category;
+
+console.log("Selected:",category);
+
+// Products filtering will be connected later.
+
+});
+
+});
