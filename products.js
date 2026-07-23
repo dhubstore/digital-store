96a0ad226b9ded7760ee8833449fcdb23e926882
Digital Hub <akangbekamal06@gmail.com>
2026-07-23 17:26:07 +0000

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,67 @@
+const products = [
 
+{
+id:1,
+name:"Netflix Premium",
+category:"Streaming",
+price:35,
+image:"images/netflix.png"
+},
+
+{
+id:2,
+name:"Spotify Premium",
+category:"Streaming",
+price:40,
+image:"images/spotify.png"
+},
+
+{
+id:3,
+name:"Express VPN",
+category:"VPN",
+price:45,
+image:"images/express-vpn.jpg"
+},
+
+{
+id:4,
+name:"Nord VPN",
+category:"VPN",
+price:45,
+image:"images/nord-vpn.png"
+},
+
+{
+id:5,
+name:"PIA VPN",
+category:"VPN",
+price:45,
+image:"images/pia-vpn.png"
+},
+
+{
+id:6,
+name:"Snapchat+",
+category:"Social",
+price:30,
+image:"images/snapchat.png"
+},
+
+{
+id:7,
+name:"Apple Music",
+category:"Streaming",
+price:40,
+image:"images/apple-music.png"
+},
+
+{
+id:8,
+name:"TextNow",
+category:"Messaging",
+price:25,
+image:"images/textnow.png"
+}
+
+];
