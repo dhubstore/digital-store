3d7a498446861c3d7e182895a79b393ecb81f14f
Digital Hub <akangbekamal06@gmail.com>
2026-07-23 17:26:45 +0000

    Update app.js

--- app.js
+++ app.js
@@ -15,3 +15,71 @@ console.log("Selected:",category);
 });
 
 });
+
+function loadProducts(){
+
+const container=document.getElementById("productsGrid");
+
+if(!container) return;
+
+container.innerHTML="";
+
+products.forEach(product=>{
+
+container.innerHTML+=`
+
+<div class="product-card">
+
+<div class="product-image">
+
+<img src="${product.image}" alt="${product.name}">
+
+</div>
+
+<div class="product-info">
+
+<div class="product-category">
+
+${product.category}
+
+</div>
+
+<h3 class="product-title">
+
+${product.name}
+
+</h3>
+
+<div class="product-price">
+
+GHS ${product.price}
+
+</div>
+
+<div class="product-buttons">
+
+<button class="add-cart">
+
+Add to Cart
+
+</button>
+
+<button class="buy-now">
+
+Buy Now
+
+</button>
+
+</div>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
+
+document.addEventListener("DOMContentLoaded",loadProducts);
