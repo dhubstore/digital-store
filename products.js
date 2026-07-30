c0d53212d08186a39ce3d89ac72d607591cbc9fa
Digital Hub <akangbekamal06@gmail.com>
2026-07-30 13:38:56 +0000

    Update products.js

--- products.js
+++ products.js
@@ -1,5 +1,7 @@
 const products = [
 
+const products = [
+
 {
 id:1,
 name:"Netflix Premium",
@@ -62,6 +64,198 @@ name:"TextNow",
 category:"Messaging",
 price:25,
 image:"images/textnow.png"
+},
+
+{
+id:9,
+name:"Disney+",
+category:"Streaming",
+price:40,
+image:"images/disney-plus.png"
+},
+
+{
+id:10,
+name:"Amazon Prime Video",
+category:"Streaming",
+price:45,
+image:"images/prime-video.png"
+},
+
+{
+id:11,
+name:"YouTube Premium",
+category:"Streaming",
+price:35,
+image:"images/youtube-premium.png"
+},
+
+{
+id:12,
+name:"Crunchyroll Premium",
+category:"Streaming",
+price:35,
+image:"images/crunchyroll.png"
+},
+
+{
+id:13,
+name:"HBO Max",
+category:"Streaming",
+price:45,
+image:"images/hbo-max.png"
+},
+
+{
+id:14,
+name:"Paramount+",
+category:"Streaming",
+price:35,
+image:"images/paramount-plus.png"
+},
+
+{
+id:15,
+name:"Surfshark VPN",
+category:"VPN",
+price:40,
+image:"images/surfshark.png"
+},
+
+{
+id:16,
+name:"CyberGhost VPN",
+category:"VPN",
+price:40,
+image:"images/cyberghost.png"
+},
+
+{
+id:17,
+name:"Proton VPN",
+category:"VPN",
+price:40,
+image:"images/proton-vpn.png"
+},
+
+{
+id:18,
+name:"TextFree",
+category:"Messaging",
+price:20,
+image:"images/textfree.png"
+},
+
+{
+id:19,
+name:"TextPlus",
+category:"Messaging",
+price:20,
+image:"images/textplus.png"
+},
+
+{
+id:20,
+name:"Google One 100GB",
+category:"Cloud",
+price:25,
+image:"images/google-one.png"
+},
+
+{
+id:21,
+name:"iCloud+ 50GB",
+category:"Cloud",
+price:52,
+image:"images/icloud.png"
+},
+
+{
+id:22,
+name:"iCloud+ 200GB",
+category:"Cloud",
+price:122,
+image:"images/icloud.png"
+},
+
+{
+id:23,
+name:"PlayStation Gift Card",
+category:"Gift Cards",
+price:100,
+image:"images/playstation.png"
+},
+
+{
+id:24,
+name:"Steam Gift Card",
+category:"Gift Cards",
+price:100,
+image:"images/steam.png"
+},
+
+{
+id:25,
+name:"Google Play Gift Card",
+category:"Gift Cards",
+price:100,
+image:"images/google-play.png"
+},
+
+{
+id:26,
+name:"Apple Gift Card",
+category:"Gift Cards",
+price:100,
+image:"images/apple-gift-card.png"
+},
+
+{
+id:27,
+name:"MTN 5GB Data",
+category:"Data",
+price:25,
+image:"images/mtn.png"
+},
+
+{
+id:28,
+name:"Telecel 5GB Data",
+category:"Data",
+price:25,
+image:"images/telecel.png"
+},
+
+{
+id:29,
+name:"AirtelTigo 5GB Data",
+category:"Data",
+price:25,
+image:"images/airteltigo.png"
+},
+
+{
+id:30,
+name:"USA Facebook Account",
+category:"Accounts",
+price:20,
+image:"images/facebook-account.png"
+},
+
+{
+id:31,
+name:"Verified Gmail Account",
+category:"Accounts",
+price:15,
+image:"images/gmail.png"
+},
+
+{
+id:32,
+name:"TikTok Account",
+category:"Accounts",
+price:20,
+image:"images/tiktok.png"
 }
 
 ];
